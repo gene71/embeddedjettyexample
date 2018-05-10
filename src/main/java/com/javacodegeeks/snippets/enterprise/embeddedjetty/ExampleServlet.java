2ddc88413043fcbe377772494025abad7cd6a9bd
gene <gene.moore71@gmail.com>
2018-05-10 14:44:32 -0600

    test commit in main

--- src/main/java/com/javacodegeeks/snippets/enterprise/embeddedjetty/ExampleServlet.java
+++ src/main/java/com/javacodegeeks/snippets/enterprise/embeddedjetty/ExampleServlet.java
@@ -1,6 +1,10 @@
 package com.javacodegeeks.snippets.enterprise.embeddedjetty;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.net.URISyntaxException;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -15,7 +19,30 @@ public class ExampleServlet extends HttpServlet {
             throws IOException {
 
         resp.setStatus(HttpStatus.OK_200);
-        resp.getWriter().println("EmbeddedJetty");
+        try {
+            resp.getWriter().println(readFile());
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String readFile() throws IOException, URISyntaxException {
+        File file = new File(getClass().getResource("example.html").toURI());
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        String         line = null;
+        StringBuilder  stringBuilder = new StringBuilder();
+        String         ls = System.getProperty("line.separator");
+
+        try {
+            while((line = reader.readLine()) != null) {
+                stringBuilder.append(line);
+                stringBuilder.append(ls);
+            }
+
+            return stringBuilder.toString();
+        } finally {
+            reader.close();
+        }
     }
 }
 
